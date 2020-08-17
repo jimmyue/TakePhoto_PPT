9b0d66d543eaf2d9c705d5f54f36a7368147e486
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2020-08-17 15:09:46 +0800

    Update local_ppt.py

--- local_ppt.py
+++ local_ppt.py
@@ -9,7 +9,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from pptx import Presentation
 from pptx.util import Inches, Pt
 
